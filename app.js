e232a48a844adce1f3461e1ac526d2cfb750366f
Gustaf Delport <79566182+Jordache007@users.noreply.github.com>
2021-09-28 13:19:37 +0200

    Migration Progress: Complete
    Migrated all final code from dev Git to Final Git

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors');
+const app = express()
+
+const corsOptions = {
+    origin: process.env.ORIGIN || "http://localhost:5000"
+};
+
+app.use(cors(corsOptions));
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true,useUnifiedTopology: true})
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log("Connected to Database"))
+
+app.use(express.json())
+
+const SensorRouter = require('./routes/dataPoints.js')
+app.use('/dataPoints',SensorRouter)
+
+app.listen(3001, () => console.log("The server has started"))
---
+++ routes/dataPoints.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const SensorData = require('../models/sensor_data')
+
+const router = express.Router()
+
+router.get('/all',async (req,res) => {
+
+    try {
+         const Data = await SensorData.find()
+         if (!(Data)) {
+              res.status(404).json({message: "No Data was found"})
+         }
+         else res.json(Data)
+         
+    } 
+    catch (error) {
+         res.status(500).json({ message: error.message})
+    }
+})
+
+//No use yet
+router.post('/send',async (req,res) => {
+     
+    const Data = new SensorData({
+         Time: req.body.Time,
+         Temperature: req.body.Temperature, 
+         HumidityPercentage: req.body.HumidityPercentage, 
+         MoisturePercentage: req.body.MoisturePercentage, 
+         LightIndex: req.body.LightIndex    
+    })
+
+    try {
+         const newData = await Data.save()
+         res.status(201).json(newData)
+
+    } catch (error) {
+         res.status(400).json({ message: error.message})
+    }
+})
+
+router.post('/point',async (req,res) => {
+    console.log(req);
+})
+
+module.exports = router
